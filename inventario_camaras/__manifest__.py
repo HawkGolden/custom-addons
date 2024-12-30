4896434f7cb6d0a67c46ff3b237f7560a06566ed
Camilo <cristiancisse12@protonmail.com>
2024-12-30 08:34:09 -0500

    Agregado archivo css en static/src/css/custom_styles.css

--- inventario_camaras/__manifest__.py
+++ inventario_camaras/__manifest__.py
@@ -5,16 +5,18 @@
     'summary': 'Gestión de cámaras de seguridad y su inventario asociado.',
     'author': 'Tu nombre',
     'website': 'http://www.tuwebsite.com',
-    'depends': ['base'],
+    'depends': ['base', 'web'],  # Agregar 'web' para usar assets en vistas
     'data': [
         'security/ir.model.access.csv',  # Habilitar el archivo de seguridad
         'views/views.xml',  # Vistas del módulo
         'views/templates.xml',  # Plantillas adicionales (opcional)
     ],
+    'assets': {
+        'web.assets_backend': [
+            'inventario_camaras/static/src/css/custom_styles.css',  # Ruta al archivo CSS
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
 }
-
-
-
